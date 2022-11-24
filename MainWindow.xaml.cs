079cea92ebf72ec0e01ed86ee1caee2aca149558
Andraoz85 <andraoz85@gmail.com>
2022-11-24 14:36:22 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace ATMApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (userInputAccountNumber.Text != "1234" || userInputPincodeNumber.Password != "9999")
+            {
+                MessageBox.Show("Wrong details. Please try again");
+            }
+            else
+            {
+                MessageBox.Show("You are logged in!");
+            }
+        }
+    }
+}
