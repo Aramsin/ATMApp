3a78db7ef630554819b2ab78070e019bed8774b8
TomasLehtela <friscobrodet@gmail.com>
2022-11-29 17:42:33 +0100

    Added childviews

---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.Models
+{
+    public class UserModel
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public decimal AccountBalance { get; set; }
+
+    }
+}
--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Caliburn.Micro;
+﻿using ATMApp.Models;
+using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,77 @@ using System.Threading.Tasks;
 
 namespace ATMApp.ViewModels
 {
-    public class ShellViewModel : Screen // (Caliburn.Micro.)Screen allows more control over opening and closing
+    public class ShellViewModel : Conductor<object>
     {
+		private int _username;
+		private string _password;
+		private UserModel _selectedUser;
+		private BindableCollection<UserModel> _users = new BindableCollection<UserModel>();
 
-    }
+		public ShellViewModel()
+		{
+			Users.Add(new UserModel { username = "admin", password = "password" });
+			Users.Add(new UserModel { username = "latti", password = "test123" });
+		}
+
+		public int Username
+		{
+			get 
+			{ 
+				return _username; 
+			}
+			set 
+			{ 
+				_username = value; 
+				NotifyOfPropertyChange(() => Username);
+			}
+		}
+
+		public string Password
+		{
+			get 
+			{ 
+				return _password; 
+			}
+			set 
+			{ 
+				_password = value; 
+				NotifyOfPropertyChange(() => Password);
+			}
+		}
+
+		public BindableCollection<UserModel> Users
+		{
+			get { return _users; }
+			set { _users = value; }
+		}
+
+		public UserModel SelectedUser
+		{
+			get { return _selectedUser; }
+			set 
+			{ 
+				_selectedUser = value; 
+				NotifyOfPropertyChange(() => SelectedUser);
+			}
+		}
+
+		//public bool CanLogin(string username, string password) => 
+		//	!String.IsNullOrWhiteSpace(username) || !String.IsNullOrWhiteSpace(password);
+
+		public void LoadLoggedIn()
+		{
+			ActivateItem(new LoggedInViewModel());
+		}
+		
+		public void LoadDeposit()
+		{
+			ActivateItem(new DepositViewModel());
+		}
+
+		public void LoadWithdraw()
+		{
+			ActivateItem(new WithdrawViewModel());
+		}
+	}
 }
